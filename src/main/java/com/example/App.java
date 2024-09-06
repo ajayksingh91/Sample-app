93a313ae8494e149547d9e273d8c60fa3b8a1efa
Ajay Kumar Singh <A-10500@ibsplc.com>
2024-09-06 10:36:53 +0530

    code update

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,7 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
-    }
+    public static void main(String[] args) { }
 
     public int add(int b, int c) {
         return b + c;
